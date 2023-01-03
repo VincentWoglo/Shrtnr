6bb8aa3d24ceeed055cec77a9181f8e288a72b5e
VincentWoglo <vincentwoglo@gmail.com>
2023-01-03 08:43:26 -0700

    Fixed and refactor  router

--- Router/Loader.php
+++ Router/Loader.php
@@ -1,29 +1,3 @@
 <?php
-    namespace Router;
-    use PHPUnit\Framework\Constraint\FileExists;
-
-    class Loader{
-
-        //Takes desired directory and path to check if file  in desired directory
-        //file_exists returns a bool
-        private static function Load($Directory, $Path ){
-            $FullPath = __DIR__.'/../'.$Directory.'/'.$Path.'.php';
-
-            if(file_exists($FullPath))
-                require_once($FullPath);
-            else
-                self::View('Views','404');
-        }
-
-        //Refactor Controller and View to be one function
-        public static function Controller($Directory, $Path, $Data = []){
-            extract($Data);
-            self::Load($Directory, $Path);
-        }
-
-        public static function View($Directory, $Path, $Data = []){
-            extract($Data);
-            self::Load($Directory, $Path);
-        }
-    }
+    //loads view to route and controller to view
 ?>
